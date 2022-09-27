bc921bfe03644f4b0294ac7b5c63c42e7d603771
@MarioDguez <marioalejandrodguez@gmail.com>
2022-09-27 18:59:48 -0500

    feat: Save and Delete records

--- Serie_de_fibonacci.py
+++ Serie_de_fibonacci.py
@@ -1,3 +1,13 @@
+import sqlite3
+import pandas as pd
+
+file="register_requests.db"
+
+connection = sqlite3.connect(file)
+cursor = connection.cursor()
+cursor.execute('CREATE TABLE IF NOT EXISTS register (Numero integer PRIMARY KEY, Serie text NOT NULL)')
+connection.commit()
+connection.close()
 
 def serie_fibonacci(limite:int=None):
     if limite<0:
@@ -7,4 +17,23 @@ def serie_fibonacci(limite:int=None):
         new_element=sum(serie[-2:])
         serie.append(new_element)
     
+    records(limite,serie[1:])
+
     return(serie[1:])
+
+def records(num:int=None,serie:list=None,save:bool=True):
+
+    connection = sqlite3.connect(file)
+    cursor = connection.cursor()
+    if save:
+        query=f'INSERT INTO register VALUES ("{num}","{serie}")'
+    else:
+        query=f'DELETE From register Where Numero = {num}'
+
+    print(query)
+
+    cursor.execute(query)
+    connection.commit()
+    connection.close()
+    
+    return None
